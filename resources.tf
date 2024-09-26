8a445868219689a3e96eaee411d6c78ac4d8f6a5
Aleksei Myslivets <amyslivets@gmail.com>
2024-09-26 14:22:40 +0200

    add DescribeContinuousBackups

--- resources.tf
+++ resources.tf
@@ -124,7 +124,8 @@ resource "aws_iam_policy" "terraform_dynamodb_access" {
           "dynamodb:PutItem",
           "dynamodb:GetItem",
           "dynamodb:DeleteItem",
-          "dynamodb:DescribeTable"
+          "dynamodb:DescribeTable",
+          "dynamodb:DescribeContinuousBackups"
         ]
         Resource  = "arn:aws:dynamodb:eu-central-1:864899869895:table/${var.terraform_state_lock_table_name}"
       },
