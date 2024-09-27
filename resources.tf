1ea22d7c5c3e9f1c67c26d66c809983e07a53343
Aleksei Myslivets <amyslivets@gmail.com>
2024-09-27 14:14:11 +0200

    refactor: remove commented tasks

--- resources.tf
+++ resources.tf
@@ -63,47 +63,13 @@ data "aws_iam_policy_document" "github_actions_assume_role_policy" {
     condition {
       test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
-      # values   = ["repo:lexxnsk/rsschool-devops-course-tasks:*"]
-      values   = ["repo:lexxnsk/rsschool-devops-course-tasks:ref:refs/heads/*"] # Uncomment it to make condition more strict
+      values   = ["repo:lexxnsk/rsschool-devops-course-tasks:*"]
+      # values   = ["repo:lexxnsk/rsschool-devops-course-tasks:ref:refs/heads/dev"] # Uncomment it to make condition more strict
     }
   }
 }
 
 # Attach Required Policies to the IAM role
-# resource "aws_iam_role_policy_attachment" "ec2_full_access" {
-#   role       = aws_iam_role.terraform_github_actions_role.name
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
-# }
-
-# resource "aws_iam_role_policy_attachment" "route53_full_access" {
-#   role       = aws_iam_role.terraform_github_actions_role.name
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonRoute53FullAccess"
-# }
-
-# resource "aws_iam_role_policy_attachment" "s3_full_access" {
-#   role       = aws_iam_role.terraform_github_actions_role.name
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
-# }
-
-# resource "aws_iam_role_policy_attachment" "iam_full_access" {
-#   role       = aws_iam_role.terraform_github_actions_role.name
-#   policy_arn = "arn:aws:iam::aws:policy/IAMFullAccess"
-# }
-
-# resource "aws_iam_role_policy_attachment" "vpc_full_access" {
-#   role       = aws_iam_role.terraform_github_actions_role.name
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonVPCFullAccess"
-# }
-
-# resource "aws_iam_role_policy_attachment" "sqs_full_access" {
-#   role       = aws_iam_role.terraform_github_actions_role.name
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonSQSFullAccess"
-# }
-
-# resource "aws_iam_role_policy_attachment" "eventbridge_full_access" {
-#   role       = aws_iam_role.terraform_github_actions_role.name
-#   policy_arn = "arn:aws:iam::aws:policy/AmazonEventBridgeFullAccess"
-# }
 resource "aws_iam_role_policy_attachment" "required_iam_policies" {
   for_each   = toset(var.required_iam_policies)
   role       = aws_iam_role.terraform_github_actions_role.name
